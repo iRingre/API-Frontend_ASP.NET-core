85148c6a87deb471c47ecbd4f1c1fb7abe75f812
iRingre <leoringressi@gmail.com>
2026-05-04 15:56:45 +0200

    aggiunta fetch clienti

--- ASP.NET/FrontEnd/Components/Services/Clients.cs
+++ ASP.NET/FrontEnd/Components/Services/Clients.cs
@@ -13,26 +13,34 @@ public class Clients
     {
         string connString = _provider.GetConnectionString();
         var con = new FbConnection(connString);
+        string sql = @"SELECT * FROM CLIENTI ORDER BY ID_CLIENTE";
 
-        var cmd = new FbCommand();
+        var cmd = new FbCommand(sql, con);
         List<Client> ListOfClient = new List<Client>();
-        ListOfClient.Add(new Client(1,"Ωmega","Pollo Primordiale","cliente1@email.com","+39 000000001"));
-        ListOfClient.Add(new Client(2,"Arciduca","Spatola Quantistica","cliente2@email.com","+39 000000002"));
-        ListOfClient.Add(new Client(3,"Entità 404","Senza Forma","cliente3@email.com","+39 000000003"));
-        ListOfClient.Add(new Client(4,"Nonna","Antimateria","cliente4@email.com","+39 000000004"));
-        ListOfClient.Add(new Client(5,"Profeta","del Router Eterno","cliente5@email.com","+39 000000005"));
-        ListOfClient.Add(new Client(6,"Imperatore","dei Bug Ancestrali","cliente6@email.com","+39 000000006"));
-        ListOfClient.Add(new Client(7,"Cavaliere","del Mouse Perduto","cliente7@email.com","+39 000000007"));
-        ListOfClient.Add(new Client(8,"Archivista","del Tempo Cache","cliente8@email.com","+39 000000008"));
-        ListOfClient.Add(new Client(9,"Oracolo","della Stampante Furiosa","cliente9@email.com","+39 000000009"));
-        ListOfClient.Add(new Client(10,"Sindaco","del Database Abissale","cliente10@email.com","+39 000000010"));
-        ListOfClient.Add(new Client(11,"Astronauta","del Desktop Remoto","cliente11@email.com","+39 000000011"));
-        ListOfClient.Add(new Client(12,"Bibliotecario","dei Log Eterni","cliente12@email.com","+39 000000012"));
-        ListOfClient.Add(new Client(13,"Domatore","di Container Ribelli","cliente13@email.com","+39 000000013"));
-        ListOfClient.Add(new Client(14,"Custode","del Firewall Mistico","cliente14@email.com","+39 000000014"));
-        ListOfClient.Add(new Client(15,"Viaggiatore","della RAM Onirica","cliente15@email.com","+39 000000015"));
-        ListOfClient.Add(new Client(16,"Strongest","soldier","cliente16@email.com","+39 000000016"));
+        con.Open();
 
+
+        using (FbDataReader reader = cmd.ExecuteReader())
+        {
+            while (reader.Read())
+            {
+                Client t = new Client
+                (
+                    reader.GetInt32(0),
+                    reader.IsDBNull(1) ? string.Empty : reader.GetString(1),
+                    reader.IsDBNull(2) ? string.Empty : reader.GetString(2),
+                    reader.IsDBNull(3) ? string.Empty : reader.GetString(3),
+                    reader.IsDBNull(4) ? string.Empty : reader.GetString(4),
+                    reader.IsDBNull(5) ? string.Empty : reader.GetString(5),
+                    reader.IsDBNull(6) ? string.Empty : reader.GetString(6),
+                    reader.GetInt32(7),
+                    reader.GetDateTime(8),
+                    reader.GetBoolean(9)
+                );
+                ListOfClient.Add(t);
+            }
+
+        }
 
         await con.CloseAsync();
         return ListOfClient;
@@ -48,14 +56,25 @@ public class Client
     public string Cognome { get; set; }
     public string Email { get; set; }
     public string Telefono { get; set; }
+    public string Indirizzo { get; set; }
+    public string Citta { get; set; }
+    public int Cap {  get; set; }
+    public DateTime Registrazione { get; set; }
+    public bool Enabled { get; set; }
+
 
     [JsonConstructor]
-    public Client(int id, string nome, string cognome, string email, string telefono)
+    public Client(int id, string nome, string cognome, string email, string telefono, string indirizzo, string citta, int cap, DateTime registrazione, bool enabled)
     {
         Id = id;
         Nome = nome;
         Cognome = cognome;
         Email = email;
         Telefono = telefono;
+        Indirizzo = indirizzo;
+        Citta = citta;
+        Cap = cap;
+        Registrazione = registrazione;
+        Enabled = enabled;
     }
 }
